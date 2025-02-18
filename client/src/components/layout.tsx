88f6832f7269d3aaef502cfa05ffcdc7536271c9
jononovo <4917673-jononovo@users.noreply.replit.com>
2025-02-18 11:19:30 +0000

    Agent query: Can you see the 5 eggs above the footer, with the first egg animating and transforming into a hatching chick and then a chick?
    Add egg animation component above footer for testing.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/bbe05200-feb1-464c-ac9f-fb48ff5de596/957381ff-fab5-4b01-a924-77f32eddee2f.jpg

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "@/components/ui/footer";
+import { EggAnimation } from "@/components/egg-animation";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,6 +11,9 @@ export function Layout({ children }: LayoutProps) {
       <div className="flex-1">
         {children}
       </div>
+      <div className="flex justify-center py-4">
+        <EggAnimation />
+      </div>
       <Footer />
     </div>
   );
@@ -20,6 +24,9 @@ export function AuthLayout({ children }: LayoutProps) {
     <div className="min-h-screen flex flex-col">
       <div className="flex-1">
         {children}
+      </div>
+      <div className="flex justify-center py-4">
+        <EggAnimation />
       </div>
       <Footer />
     </div>
